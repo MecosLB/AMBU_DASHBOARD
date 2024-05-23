115a32ed376b5191c4453033dc74edaa010054cb
Sebastian CHG <sebokiabot@gmail.com>
2024-05-23 08:37:55 -0600

    Dependencies added and bootstrap set up

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './assets/styles/main.scss';
+import * as bootstrap from 'bootstrap';
+import Login from './views/Login';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Login />
   </React.StrictMode>,
 )
